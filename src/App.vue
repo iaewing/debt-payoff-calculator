3521916d8541bd3fd465f2b9324830cdc8507a93
Andrew Noble <a.noble@vehikl.com>
2021-11-12 16:53:16 -0500

    Create card component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,51 @@
 <template>
   <NavBar />
+  <div class="grid grid-cols-3">
+    <Card class="bg-purple-500 text-white p-4">
+    <template v-slot:default>
+      $45,000.00
+    </template>
+    <template v-slot:header>
+      <div>Principal Paid</div>
+    </template>
+    <template v-slot:subtext>
+      Wow im broke
+    </template>
+  </Card>
+    <Card class="bg-yellow-600 text-white p-4">
+      <template v-slot:default>
+        $45,000.00
+      </template>
+      <template v-slot:header>
+        <div>Principal Paid</div>
+      </template>
+      <template v-slot:subtext>
+        Wow im broke
+      </template>
+    </Card>
+    <Card class="bg-red-500 text-white p-4">
+      <template v-slot:default>
+        $45,000.00
+      </template>
+      <template v-slot:header>
+        <div>Principal Paid</div>
+      </template>
+      <template v-slot:subtext>
+        Wow im broke
+      </template>
+    </Card>
+  </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
     NavBar,
+    Card,
   }
 }
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col max-w-2xl ">
+    <div class="text-lg">
+      <slot name="header" />
+    </div>
+    <div class="text-3xl font-bold">
+      <slot></slot>
+    </div>
+    <div>
+      <slot name="subtext"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card"
+}
+</script>
