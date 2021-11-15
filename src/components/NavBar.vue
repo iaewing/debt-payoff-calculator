929e64b4e91355555efcc028cc0e13fe84a54cda
ian-apprentice-Vehikl <ian.vehikl@apprenticefs.com>
2021-11-15 15:58:25 -0500

    Change navbar name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full bg-blue-500 p-4 flex justify-between">
     <div>
-      <h1 class="text-white text-xl">Unbury.us</h1>
+      <h1 class="text-white text-xl">Totally Original Debt Calculator</h1>
     </div>
     <div class="flex">
       <div class="px-8 text-white uppercase">Login</div>
