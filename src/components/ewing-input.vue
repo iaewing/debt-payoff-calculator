19977e165cb3abd02b01c61e3d7c7c5fdbc2f2b5
Andrew Noble <a.noble@vehikl.com>
2021-12-08 16:51:22 -0500

    extract ewing-input component

---
+++ src/components/ewing-input.vue
@@ -0,0 +1,31 @@
+<template>
+  <label :for="$attrs.id">{{ $attrs.placeholder }}</label>
+  <input
+      v-bind="$attrs"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="border-0 border-b border-gray-500 pl-0"
+  />
+  <span class="text-red-600">{{ error }}</span>
+</template>
+
+<script>
+export default {
+  name: "ewing-input",
+  inheritAttrs: false,
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    error: {
+      type: String,
+      default: ""
+    }
+  },
+  emits: ['update:modelValue']
+}
+</script>
+
+<style scoped>
+
+</style>
